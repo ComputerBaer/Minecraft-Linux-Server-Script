fd392520a2b169e8f24b528cad12239faa813b17
ComputerBaer <computerbaer@gmail.com>
2016-01-29 13:31:26 +0100

    Actions, Language and Configuration

---
+++ .script/game.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+GAME_IS_RUNNING=false
+
+# InstallGame Function
+function InstallGame
+{
+    return # Empty Function
+}
+
+# StartGame Function
+function StartGame
+{
+    return # Empty Function
+}
+
+# StopGame Function
+function StopGame
+{
+    return # Empty Function
+}
+
+# GameStatus Function
+function GameStatus
+{
+    return # Empty Function
+}
+
+# BackupGame Function
+function BackupGame
+{
+    return # Empty Function
+}
