7df59c013e5010cf371f96142d85f1d0e7821d6d
ComputerBaer <computerbaer@gmail.com>
2016-01-29 14:42:25 +0100

    Install Game and Minecraft EULA Check

--- .script/game.sh
+++ .script/game.sh
@@ -2,10 +2,49 @@
 
 GAME_IS_RUNNING=false
 
+# CheckEula Function
+function CheckEula
+{
+    eula=false
+    if [ -f $GAME_EULA ]; then
+        source $GAME_EULA
+    fi
+    eula=$(CheckBoolean $eula false)
+
+    if [[ $eula == true ]]; then
+        return
+    fi
+
+    echo -e "${FG_YELLOW}${STR_GAME_EULA}${RESET_ALL}"
+    echo -e "${FG_YELLOW}${GAME_EULA_URL}${RESET_ALL}"
+    if [[ $(DialogYesNo) == true ]]; then
+        echo "eula=true" > $GAME_EULA
+    fi
+}
+
 # InstallGame Function
 function InstallGame
 {
-    return # Empty Function
+    if [ ! -d $GAME_DIR ]; then
+        mkdir -p $GAME_DIR
+    fi
+
+    if [ -f $GAME_EXECUTABLE ]; then
+        rm $GAME_EXECUTABLE
+    fi
+
+    echo -e "${FG_YELLOW}${STR_GAME_INSTALL_START}${RESET_ALL}"
+    curl -s $GAME_DL_URL -o $GAME_EXECUTABLE &
+    WaitForBackgroundProcess $! $FG_YELLOW
+
+    local size=$(stat --format=%s $GAME_EXECUTABLE)
+    if [ $size -lt 1000 ]; then
+        echo -e "${FG_RED}${STR_GAME_INSTALL_FAILED}${RESET_ALL}"
+        return
+    fi
+    echo -e "${FG_GREEN}${STR_GAME_INSTALL_DONE}${RESET_ALL}"
+
+    CheckEula
 }
 
 # StartGame Function
